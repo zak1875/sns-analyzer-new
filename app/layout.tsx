70395d4a99ee9fbc45d54e69c03cb45b0b9f414f
zak1875 <yusukeysk81@gmail.com>
2025-03-21 23:20:25 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
+      <head>
+        <title>SNS診断アプリ</title>
+      </head>
       <body>{children}</body>
     </html>
   );
